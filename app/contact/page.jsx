ed180be599d79f3005d023401af4d629aa24df60
Sarvesh Kadam <147650579+SARVESH-31@users.noreply.github.com>
2024-08-21 05:57:12 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
               I’m always open to exciting collaborations and projects. Whether you need a robust web solution, cloud consultancy, or help bringing your ideas to life, I’m here to assist. Let’s connect and create something great together!
               </p>
