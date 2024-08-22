afbe7d35ee93f5ec89d988821d0494c0747f8665
Sarvesh Kadam <sarveshdadasaheb.kadam2021@vitstudent.ac.in>
2024-08-22 13:58:04 +0530

    Title changed

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Sarvesh Kadam</span>
+              Hello I&apos;m <br /> <span className="text-accent">Sarvesh Kadam</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I specialize in creating seamless digital experiences and 
